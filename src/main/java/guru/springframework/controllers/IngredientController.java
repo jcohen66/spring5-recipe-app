6c60b7a005e59abd3be19ff0476287e5240ed8a6
jcohen66 <jcohen66@optonline.net>
2019-01-20 16:00:40 -0500

    Added an ingredients controller and tests.

--- src/main/java/guru/springframework/controllers/IngredientController.java
+++ src/main/java/guru/springframework/controllers/IngredientController.java
@@ -1,9 +1,11 @@
 package guru.springframework.controllers;
 
+import guru.springframework.services.IngredientService;
 import guru.springframework.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -11,12 +13,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IngredientController {
 
-    RecipeService recipeService;
+    private final RecipeService recipeService;
 
-    public IngredientController(RecipeService recipeService) {
+    private final IngredientService ingredientService;
+
+    public IngredientController(RecipeService recipeService, IngredientService ingredientService) {
         this.recipeService = recipeService;
+        this.ingredientService = ingredientService;
     }
 
+    @GetMapping
     @RequestMapping("/recipe/{recipeId}/ingredients")
     public String ingredients(@PathVariable String recipeId, Model model) {
         log.debug("Listing ingredients for recipe id: " + recipeId);
@@ -24,5 +30,15 @@ public class IngredientController {
         model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(recipeId)));
 
         return "recipe/ingredient/list";
+    }
+
+    @GetMapping
+    @RequestMapping("recipe/{recipeId}/ingredient/{id}/show")
+    public String showRecipeIngredient(@PathVariable String recipeId,
+                                       @PathVariable String id,
+                                        Model model) {
+
+        model.addAttribute("ingredient", ingredientService.findByRecipeIdAndIngredientId(Long.valueOf(recipeId), Long.valueOf(id)));
+        return "recipe/ingredient/show";
     }
 }
